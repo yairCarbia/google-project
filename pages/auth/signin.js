47075d29d2885aa3db1b2d70ce81c1df2b8e00d3
zarpadoen <yaircarbia420@gmail.com>
2022-07-18 20:46:57 -0300

    finalizado

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -14,7 +14,7 @@ export default function signin({ providers }) {
                             src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
                             alt="google-logo"
                         />
-                        <p className="text-sm italic my-10 text-center">This website is created for learning purposes</p>
+                        <p className="text-sm italic my-10 text-center">Sitio creado con propositos de aprendisaje , esta pagina no es la real.</p>
                         <button className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-500" onClick={() => signIn(provider.id, { callbackUrl: "/" })}>
                             Sign in with {provider.name}
                         </button>
